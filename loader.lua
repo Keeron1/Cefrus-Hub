e8a4ec2625f49d47ab3626a9c5421eab57aa7161
Keeron1 <keeron9@gmail.com>
2023-04-12 23:54:51 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,6 @@
 if game.Placeid == 12413786484 then --anime lost simulator
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/animelostsimulator.lua"))()
+    print("Loading Anime Lost Simulator!")
+else
+    print("Game Not Supported!")
 end
