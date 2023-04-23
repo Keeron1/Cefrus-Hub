cd333e7f865e4839e3159d15d79bd689d2f4b41e
Keeron1 <keeron9@gmail.com>
2023-04-23 20:27:16 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,6 +5,8 @@ elseif game.PlaceId == 12135645852 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/animecatchingsimulator.lua"))()
 elseif game.PlaceId == 8069117419 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/demonsoulsimulator.lua"))()
+elseif game.PlaceId == 12980045478 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/wallsmashsimulator.lua"))()
 else
     print("Game Not Supported!")
 end
