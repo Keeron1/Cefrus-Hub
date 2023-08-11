f72c55d793f65c36f96c04cebbc860eec0882dff
Keeron1 <keeron9@gmail.com>
2023-08-11 20:21:21 +0200

    released anime warriors 2 script

--- loader.lua
+++ loader.lua
@@ -1,5 +1,4 @@
-print("loader executed")
-if game.PlaceId == 12413786484 then --anime lost simulator
+if game.PlaceId == 12413786484 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/animelostsimulator.lua"))()
 elseif game.PlaceId == 12135645852 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/animecatchingsimulator.lua"))()
@@ -13,6 +12,8 @@ elseif game.PlaceId == 12986400307 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/swordwarriors.lua"))()
 elseif game.PlaceId == 11445923563 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/onefruit.lua"))()
+elseif game.PlaceId == 8425128940 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Keeron1/Cefrus-Hub/main/animewarriorssimulator2.lua"))()
 else
     print("Game Not Supported!")
 end
